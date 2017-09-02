b065f076e04e74a8ce36a7afbf10a24af92bd07e
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2017-09-02 19:15:05 +0300

    Implemented redux architecture, added local notification

--- app/routes/Broadcasting/components/StatusIcon.js
+++ app/routes/Broadcasting/components/StatusIcon.js
@@ -4,27 +4,39 @@ import { Icon, Spinner } from 'native-base';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 import styles from '../styles';
+import DebouncedRenderer from '../../../components/DebouncedRenderer';
 import { accentColor } from '../../../config/androidColorPallete';
 import * as playerStatus from '../../../constants/PlayerStatuses';
 
-const StatusIcon = ({ status }) => {
-  if (status === playerStatus.BUFFERING || status === playerStatus.ERROR) {
-    return <Spinner color={accentColor} />;
+export default class StatusIcon extends DebouncedRenderer {
+  static propTypes = {
+    status: PropTypes.string.isRequired,
   }
 
-  if (status === playerStatus.STOPPED) {
-    return <Icon name="ios-mic-off" style={styles.icon} />;
+  shouldComponentUpdate = (nextProps, nextState) => (
+    this.props.status !== nextProps.status ||
+    this.state !== nextState
+  )
+
+  get updateImmediate() {
+    const { status } = this.props;
+
+    return status === playerStatus.BUFFERING;
   }
 
-  if (status === playerStatus.CONNECTIONOFF) {
-    return <MaterialIcon name="signal-wifi-off" style={styles.icon} />;
+  render() {
+    const { status } = this.state;
+
+    if (status === playerStatus.BUFFERING || status === playerStatus.ERROR) {
+      return <Spinner color={accentColor} />;
+    }
+    if (status === playerStatus.STOPPED) {
+      return <Icon name="ios-mic-off" style={styles.icon} />;
+    }
+    if (status === playerStatus.CONNECTIONOFF) {
+      return <MaterialIcon name="signal-wifi-off" style={styles.icon} />;
+    }
+
+    return <Icon name="ios-radio-outline" style={styles.icon} />;
   }
-
-  return <Icon name="ios-radio-outline" style={styles.icon} />;
-};
-
-StatusIcon.propTypes = {
-  status: PropTypes.string,
-};
-
-export default StatusIcon;
+}
